5e35b6545ce02aa2db52f6deb8a311b0f5bb77b5
Murilo Marinho <murilomarinho@manchester.ac.uk>
2025-04-14 13:31:13 +0100

    Adding sphinx documentation generation.

---
+++ docs/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+from datetime import date
+#from dqrobotics_extensions.pyplot import __version__
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'dqrobotics-pyplot'
+copyright = f'2025-{date.today().year}, Murilo M. Marinho'
+author = 'Murilo M. Marinho'
+release = '25.04'
+#version = __version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+    "autodoc2",
+    #"sphinx.ext.intersphinx", # not used just yet
+    "sphinx.ext.viewcode"
+]
+autodoc2_render_plugin = "myst"
+autodoc2_packages = [
+    {
+        "path": "../src/dqrobotics_extensions/pyplot",
+    },
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+myst_enable_extensions = ["fieldlist", "deflist"]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
