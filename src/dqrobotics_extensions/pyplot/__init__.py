8303c62a70bf9face7c8f501d2faadd81a98012c
Murilo Marinho <murilomarinho@manchester.ac.uk>
2025-04-14 14:00:57 +0100

    [dqrobotics-pyplot] Adding version file as described in https://setuptools-git-versioning.readthedocs.io/en/stable/runtime_version.html

--- src/dqrobotics_extensions/pyplot/__init__.py
+++ src/dqrobotics_extensions/pyplot/__init__.py
@@ -1,1 +1,5 @@
 from dqrobotics_extensions.pyplot._pyplot import plot
+
+# https://setuptools-git-versioning.readthedocs.io/en/stable/runtime_version.html
+from pathlib import Path
+__version__ = Path(__file__).parent.joinpath("VERSION").read_text()
