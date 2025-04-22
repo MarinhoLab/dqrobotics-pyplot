b9a739576befef28ef2f031a2a27f0b1d91422d3
Murilo Marinho <murilomarinho@manchester.ac.uk>
2025-04-22 09:11:29 +0100

    [gallery] Making gallery examples easier to extend by separating each example into a separate submodule.

---
+++ src/dqrobotics_extensions/pyplot/gallery/output_doc_samples.py
@@ -0,0 +1,34 @@
+"""
+Copyright (C) 2025 Murilo Marques Marinho (www.murilomarinho.info)
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+Author: Murilo M. Marinho
+"""
+from _ouput_poses import output_poses
+from _output_lines import output_lines
+from _output_planes import output_planes
+from _output_moving_primitives import output_moving_primitives
+
+def main():
+
+    output_poses()
+    output_lines()
+    output_planes()
+    output_moving_primitives()
+
+if __name__ == "__main__":
+    main()
+
